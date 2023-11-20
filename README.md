aadf34ce1077899bd8f1283d196132d990ed496d
Ouattara Arouna <130720244+Ano2225@users.noreply.github.com>
2023-11-20 11:41:03 +0000

    Mise en ligne de mon projet Porfolio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Portfolio-Made-with-HTML-Css-Js
 Portfolio réalisé avec HTML CSS ET JS 
+
+bY Ouattara Arouna
---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* =============== typing animation ===================== */
+
+var typed = new Typed(".typing", {
+    strings:["","Web Developer","Graphic Designer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
